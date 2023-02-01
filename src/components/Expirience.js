ce17a79977321da4bd5a155e2720d9e9778ed443
Kenyi Hancco <kenyihq@gmail.com>
2023-02-01 18:51:32 -0500

    Finish component code

--- src/components/Expirience.js
+++ src/components/Expirience.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 const Expirience = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section id="experience">
+            <div class="experience-head">
+                <h1>PARTICIPACIONES</h1>
+            </div>
+            <div class="experience">
+                <div class="experience-img">
+                    <a href="https://www.linkedin.com/posts/palo-it_felicidades-a-los-ganadores-del-hackathon-activity-6827730245093871616-KUZ_" target="blank_">
+                        <img src="./img/hackathon.png" alt="Experiencia" class="img">
+                    </a>
+                </div>
+                <div class="experience-video">
+                    <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6827730156690518016?compact=1" frameborder="0" allowfullscreen="" title="Publicación integrada" class="video"></iframe>
+                </div>
+            </div>
+
+        </section>
+    )
 }
 
 export default Expirience
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,104 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="projects">
+            <div class="project-head">
+                <p>A lo largo de este tiempo he ido aprendiendo cada vez más de este mundo tan maravilloso que es la indsutria <i>tech</i>, a continuación les persento mis proyectos personales</p>
+                <h1>Aquí hay algunos que me gustaría compartir.</h1>
+                <img src="./img/arrow-down.png" alt="⬇">
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="#main"">
+                        <img src="./img/portfolio.png" alt="Portafolio persoanal" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="#main" class="title">kenyihq.com</a>
+                    <p>Portafolio personal que fue desarrollado con HTML, CSS y un guiño de Javascript, un proyecto simple pero funcional, sirvió bastante para practicar mis hablidades en desarrollo web</p>
+                    <a href="https://github.com/kenyihq/portfolio/blob/main/index.html" target="_blank" class="button2">Ver código</a>
+                    <a href="#main" class="button">Ver proyecto</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="https://replit.com/@kenyihq/pokemongame#main.py" target="_blank">
+                        <img src="./img/pokemon.png" alt="Pokemon" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="https://replit.com/@kenyihq/pokemongame#main.py" target="_blank" class="title">Lógica del juego Pokemon</a>
+                    <p>Un pequeño proyecto que fue desarrollado en Python a partir del diagrama de flujos. Se desarrolló la lógica del juego</p>
+                    <a href="https://github.com/kenyihq/pokemon" target="_blank" class="button2">Ver código</a>
+                    <a href="https://cuadernodelprogramador.blogspot.com/2021/07/pokemon.html" target="_blank" class="button">Ver proyecto</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="https://replit.com/@kenyihq/todo#main.py" target="_blank">
+                        <img src="./img/to-do.png" alt="Pokemon" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="https://replit.com/@kenyihq/todo#main.py" target="_blank" class="title">To-Do list en Python</a>
+                    <p>Un sencillo gestor de tareas (to-do), escrita en Python que corre en la terminal de línea de comandos</p>
+                    <a href="https://github.com/kenyihq/python-practice/blob/master/todo.py" target="_blank" class="button2">Ver código</a>
+                    <a href="https://cuadernodelprogramador.blogspot.com/2021/07/to-do-list-en-python.html" target="_blank" class="button">Ver proyecto</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="https://kenyihq.github.io/snake-game/" target="_blank">
+                        <img src="./img/snake.png" alt="Snake" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="https://kenyihq.github.io/snake-game/" target="_blank" class="title">Snake Game</a>
+                    <p>El clásico juego de Snake, hecho con Javascript, CSS y HTML. Tiene los movimientos básicos con teclado y un pequeño joystick para el formato móvil.</p>
+                    <a href="https://github.com/kenyihq/snake-game" target="_blank" class="button2">Ver código</a>
+                    <a href="https://kenyihq.github.io/snake-game/" target="_blank" class="button">Ver proyecto</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="https://kenyihq.github.io/videoplayer/" target="_blank">
+                        <img src="./img/videoplayer.png" alt="VideoPlayer" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="https://kenyihq.github.io/videoplayer/" target="_blank" class="title">Video Player</a>
+                    <p>El reproductor de video simple y sencillo, hecho con Javascript, CSS y HTML. Tiene las funciones básicas de un reproductor de video.</p>
+                    <a href="https://github.com/kenyihq/videoplayer/blob/master/index.html" target="_blank" class="button2">Ver código</a>
+                    <a href="https://kenyihq.github.io/videoplayer/" target="_blank" class="button">Ver proyecto</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="https://kenyihq.github.io/pokedex/" target="_blank">
+                        <img src="./img/pokedex.png" alt="Pokedex" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="https://kenyihq.github.io/pokedex/" target="_blank" class="title">Pokedex</a>
+                    <p>Una Pokedex simple escrita en HTML, CSS y JavaScript, consume la API de <a href="https://pokeapi.co/api/v2/pokemon">PokeAPI.co</a></p>
+                    <a href="https://github.com/kenyihq/pokedex/blob/master/index.html" target="_blank" class="button2">Ver código</a>
+                    <a href="https://kenyihq.github.io/pokedex/" target="_blank" class="button">Ver proyecto</a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-img">
+                    <a href="https://kenyihq.github.io/JS-api-php/" target="_blank">
+                        <img src="./img/crud-js.png" alt="Crud PHP" class="img">
+                    </a>
+                </div>
+                <div class="project-description">
+                    <a href="https://kenyihq.github.io/JS-api-php/" target="_blank" class="title">Crud con Javascript</a>
+                    <p>Pequeña interface para realizar un CRUD consumiendo una API en PHP, el cual usa una base de datos MySql.</p>
+                    <a href="https://github.com/kenyihq/JS-api-php/blob/master/index.html" target="_blank" class="button2">Ver código</a>
+                    <a href="https://kenyihq.github.io/JS-api-php/" target="_blank" class="button">Ver proyecto</a>
+                </div>
+            </div>
+        </section>  
   )
 }
 
