e3fb9d417c92b38786a152283cc1db59a5e8244b
Kenyi Hancco <kenyihq@gmail.com>
2023-02-01 18:48:45 -0500

    Added components

---
+++ src/components/Expirience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Expirience = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Expirience
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header class="header">
+            <a href="#index">
+                <img src="./img/logo.png" alt="Logo" width="155">
+            </a>
+            <nav class="menu" aria-label="main-navigation">
+                <ul>
+                    <li><a href="#projects">Proyectos</a></li>
+                    <li><a href="#contact">Hablemos</a></li>
+                    <li><a href="https://401u.blogspot.com/" target="_blank">Blog</a></li>
+                </ul>
+            </nav>
+            <button class="button"><a href="#certificates">Certificados</a></button>
+        </header>
+    )
+}
+
+export default header
