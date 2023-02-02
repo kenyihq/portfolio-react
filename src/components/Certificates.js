bcc59547bf04245fa42f7b9feaf79755a6fef87c
Kenyi Hancco <kenyihq@gmail.com>
2023-02-01 20:06:34 -0500

    Added CSS styles in project

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -3,20 +3,21 @@ import React from 'react';
 const Certificates = () => {
   return (
     <section id="certificates">
-            <div class="certificates-head">
-                <h1>Algunas certificaciones que me gustaría compartir</h1>
-                <img src='./img/arrow-down-white.png' alt="⬇"/>
+        <div class="certificates-head">
+            <h1>Algunas certificaciones que me gustaría compartir</h1>
+            <img src='../img/arrow-down-white.png' alt="⬇"/>
+            
+        </div>
+        <div class="certificate">
+            <div class="certificate-img">
+                <a href="https://platzi.com/p/kenyihq/curso/2508-introduccion-backend/diploma/detalle/" target="_blank"><img src="../img/certificate7.png" alt="Certificado" class="img"/></a>    
+                <a href="https://platzi.com/p/kenyihq/curso/1474-oop/diploma/detalle/" target="_blank"><img src="./img/certificate3.png" alt="Certificado" class="img"/></a>
+                <a href="https://platzi.com/p/kenyihq/curso/2255-python-intermedio/diploma/detalle/" target="_blank"><img src="../img/certificate5.png" alt="Certificado" class="img"/></a>
+                <a href="https://platzi.com/p/kenyihq/curso/1775-course/diploma/detalle/" target="_blank"><img src="../img/certificate9.png" alt="Certificado" class="img"/></a>
+                <a href="https://ed.team/@kenyihq/curso/vim" target="_blank"><img src="./img/certificate2.png" alt="Certificado" class="img"/></a>               
+                <a href="https://platzi.com/p/kenyihq/curso/1557-course/diploma/detalle/" target="_blank"><img src="../img/certificate10.png" alt="Certificado" class="img"/></a>
             </div>
-            <div class="certificate">
-                <div class="certificate-img">
-                    <a href="https://platzi.com/p/kenyihq/curso/2508-introduccion-backend/diploma/detalle/" target="_blank"><img src="../img/certificate7.png" alt="Certificado" class="img"/></a>    
-                    <a href="https://platzi.com/p/kenyihq/curso/1474-oop/diploma/detalle/" target="_blank"><img src="./img/certificate3.png" alt="Certificado" class="img"/></a>
-                    <a href="https://platzi.com/p/kenyihq/curso/2255-python-intermedio/diploma/detalle/" target="_blank"><img src="../img/certificate5.png" alt="Certificado" class="img"/></a>
-                    <a href="https://platzi.com/p/kenyihq/curso/1775-course/diploma/detalle/" target="_blank"><img src="../img/certificate9.png" alt="Certificado" class="img"/></a>
-                    <a href="https://ed.team/@kenyihq/curso/vim" target="_blank"><img src="./img/certificate2.png" alt="Certificado" class="img"/></a>               
-                    <a href="https://platzi.com/p/kenyihq/curso/1557-course/diploma/detalle/" target="_blank"><img src="../img/certificate10.png" alt="Certificado" class="img"/></a>
-                </div>
-            </div>
+        </div>
 
         </section>
   );
